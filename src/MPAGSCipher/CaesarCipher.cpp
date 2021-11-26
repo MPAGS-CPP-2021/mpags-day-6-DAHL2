3919ef5c2246c5c391dc8111e2225a1fab72653d
Dennis Lindebaum <dennis.lindebaum@bristol.ac.uk>
2021-11-26 10:57:45 +0000

    Tested for negative number input

--- src/MPAGSCipher/CaesarCipher.cpp
+++ src/MPAGSCipher/CaesarCipher.cpp
@@ -10,48 +10,37 @@ CaesarCipher::CaesarCipher(const std::size_t key) : key_{key % Alphabet::size}
 
 CaesarCipher::CaesarCipher(const std::string& key) : key_{0}
 {
-    // // We have the key as a string, but the Caesar cipher needs an unsigned long, so we first need to convert it
-    // // We default to having a key of 0, i.e. no encryption, if no (valid) key was provided on the command line
-    // if (!key.empty()) {
-    //     // Before doing the conversion we should check that the string contains a
-    //     // valid positive integer.
-    //     // Here we do that by looping through each character and checking that it
-    //     // is a digit. What is rather hard to check is whether the number is too
-    //     // large to be represented by an unsigned long, so we've omitted that for
-    //     // the time being.
-    //     // (Since the conversion function std::stoul will throw an exception if the
-    //     // string does not represent a valid unsigned long, we could check for and
-    //     // handle that instead but we only cover exceptions very briefly on the
-    //     // final day of this course - they are a very complex area of C++ that
-    //     // could take an entire course on their own!)
-    //     for (const auto& elem : key) {
-    //         if (!std::isdigit(elem)) {
-    //             std::cerr
-    //                 << "[error] cipher key must be an unsigned long integer for Caesar cipher,\n"
-    //                 << "        the supplied key (" << key
-    //                 << ") could not be successfully converted" << std::endl;
-    //             return;
-    //         }
-    //     }
-    //     key_ = std::stoul(key) % Alphabet::size;
-    // }
+    // We have the key as a string, but the Caesar cipher needs an unsigned long, so we first need to convert it
+    // We default to having a key of 0, i.e. no encryption, if no (valid) key was provided on the command line
+    if (!key.empty()) {
+        // Explicitly check the user hasn't tried to input a negative number
+        if (key.front() == '-') {
+            std::cerr
+                << "[error] cipher key must be an unsigned long integer for Caesar cipher,\n"
+                << "        the supplied key (" << key
+                << ") appears to be a negative number,\n"
+                << "        which will not be properly converted.\n"
+                << "Continuing with default key: 1" << std::endl;
+            key_ = 1;
+        }
 
-    try {
-        key_ = std::stoul(key) % Alphabet::size;
-    } catch (std::invalid_argument& e) {
-        // N.B. this error is only raised if ALL characters are not integers: it will still pass
-        //  if i.e. we type -1: This isn't ideal so the previous method of handling this might be better!
-        std::cerr
-            << "[error] cipher key must be an unsigned long integer for Caesar cipher,\n"
-            << "        the supplied key (" << key
-            << ") could not be successfully converted.\n" 
-            << "Continuing with default key: 1" << std::endl;
+        try {
+            key_ = std::stoul(key) % Alphabet::size;
+        } catch (std::invalid_argument& e) {
+            // N.B. this error is only raised if ALL characters are not integers: it will still pass
+            //  if i.e. we type "1banana": this isn't ideal so the previous method of handling this might be better!
+            std::cerr
+                << "[error] cipher key must be an unsigned long integer for Caesar cipher,\n"
+                << "        the supplied key (" << key
+                << ") could not be successfully converted.\n"
+                << "Continuing with default key: 1" << std::endl;
             key_ = 1;
-    } catch (std::out_of_range& e) {
-        std::cerr
-            << "[error] the supplied cipher key is too large to be converted to an unsiggned long.\n"
-            << "Continuing with default key: 1" << std::endl;
+        } catch (std::out_of_range& e) {
+            std::cerr
+                << "[error] the supplied cipher key is too large to be converted to an unsigned long.\n"
+                << "Continuing with default key: 1" << std::endl;
             key_ = 1;
+        }
     }
 }
 
